0de18f6d945bbda597ffceeff25127c2b09c971d
Mohit Kumar <53662024+moit-bytes@users.noreply.github.com>
2022-06-14 19:27:11 +0530

    Updated Codes 14/06

---
+++ 02_Maths/01_triangle.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#define int long long int
+using namespace std;
+
+int32_t main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		int a, b, c, d;
+		cin >> a >> b >> c >> d;
+		cout << b << " " << c << " " << c << "\n";
+
+
+	}
+	return 0;
+}
